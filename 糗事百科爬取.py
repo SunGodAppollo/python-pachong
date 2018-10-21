a9583c07e695fa30a459a40eb761ee4c27e9c7d2
狐狸 <417045639@qq.com>
2018-10-21 09:18:09 +0800

    完善内容

--- 糗事百科爬取.py
+++ 糗事百科爬取.py
@@ -7,6 +7,7 @@ Created on 2018年9月1日
 from web import Url
 from web import Zhengzhe
 import time
+import sys
 from web import Save
 
 
@@ -14,20 +15,25 @@ if __name__ == '__main__':
     #url="https://acg12.com/278708/"
     #https://www.qiushibaike.com/8hr/page/1/
     z=Zhengzhe.Zhengzhe()
-    f=open('html.txt','w',encoding="utf-8");
-    for page in range(1,20):
+    fileTime=time.strftime('%Y%m%d%H%M%S',time.localtime(time.time()))
+
+    #print(fileTime)
+    #sys.exit(0)
+
+    f=open(fileTime+'糗事百科.txt','w',encoding='utf_8_sig')
+    for page in range(1,10):
         url="https://www.qiushibaike.com/8hr/page/"+str(page)+"/"
         gethtml=Url.Url(1)
         html=gethtml.getHtml_headers(url)
-        
+
         #print(html)
-       
-        
+
+
         imgList=z.getAllqiushi(html)
-       
-        
+
+
         for i in imgList:
-            mystr=i.strip()            
+            mystr=i.strip()
             mystr=mystr.replace('<br/>','\n')
             print(mystr)
             saveStr=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))+"\n"+mystr+"\n"
@@ -35,4 +41,3 @@ if __name__ == '__main__':
     f.close()
     print("end")
     #print(html)
-    
